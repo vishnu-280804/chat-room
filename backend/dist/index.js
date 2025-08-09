aa4007f34565f95e10301cddc634b6cb2299343c
vishnu <143005718+vishnu-280804@users.noreply.github.com>
2025-08-09 14:58:23 +0530

    Added Backend Logic and frontend as a template

---
+++ backend/dist/index.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8000 });
+let map = new Map();
+wss.on("connection", (socket) => {
+    console.log("Connecting dude");
+    socket.on("message", (message) => {
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        console.log(parsedMessage);
+        if (parsedMessage.type === "join") {
+            console.log("User Joined Room");
+            const roomId = parsedMessage.payload.roomId;
+            console.log("Room Id is " + (roomId));
+            if (!map.has(roomId)) {
+                map.set(roomId, [socket]);
+            }
+            else {
+                map.get(roomId).push(socket);
+            }
+            console.log("Map is \n" + map);
+        }
+        if (parsedMessage.type === "chat") {
+            let userRoom = null;
+            let flag = false;
+            for (const [key, value] of map) {
+                if (value.includes(socket)) {
+                    userRoom = key;
+                    break;
+                }
+            }
+            if (!userRoom)
+                return;
+            console.log("User room in chat is " + (userRoom));
+            //@ts-ignore
+            const res = map.get(userRoom);
+            for (let i = 0; i < res.length; i++) {
+                res[i].send(parsedMessage.payload.message);
+                console.log("Sending dude");
+            }
+        }
+    });
+    socket.on("disconnect", (socket) => {
+        //socket.filter(x=>x!=socket);
+        for (const [key, value] of map) {
+            const updatedArray = value.filter((x) => x !== socket);
+            if (updatedArray.length === 0) {
+                map.delete(key);
+            }
+            else {
+                map.set(key, updatedArray);
+            }
+        }
+    });
+});
