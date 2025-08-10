38108522f455f3cd304399d26a227ff7d5c8a9ee
vishnu <143005718+vishnu-280804@users.noreply.github.com>
2025-08-10 19:38:06 +0530

    Added create and join room logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,14 +10,27 @@ wss.on("connection",(socket)=>{
         //@ts-ignore
         const parsedMessage = JSON.parse(message);
         console.log(parsedMessage);
-
+        // msg = {
+        //  type = create
+        //  payload 
+        // roomName = "red"
+        if(parsedMessage.type === "create"){
+            const roomId = parsedMessage.payload.roomId;
+            if(map.has(roomId)){
+                return;
+            }
+            else{
+                map.set(roomId,[socket]);
+            }
+        }
         if(parsedMessage.type === "join"){
             console.log("User Joined Room");
             const roomId = parsedMessage.payload.roomId;
             console.log("Room Id is "+(roomId));
 
             if(!map.has(roomId)){
-                map.set(roomId,[socket]);
+                return;
+                //map.set(roomId,[socket]);
             }
             else{
                 map.get(roomId)!.push(socket);
@@ -40,10 +53,16 @@ wss.on("connection",(socket)=>{
             console.log("User room in chat is "+(userRoom));
             //@ts-ignore
             const res: WebSocket[] = map.get(userRoom);
+            const chatData = {
+                type : "chat",
+                message:parsedMessage.payload.message,
+                sender:parsedMessage.payload.sender,
+                time:Date.now()
+            };
 
             for(let i=0;i<res.length;i++){
-                res[i].send(parsedMessage.payload.message);
-                console.log("Sending dude");
+                res[i].send(JSON.stringify(chatData));
+                //console.log("Sending dude");
             }
         }
 
@@ -51,7 +70,7 @@ wss.on("connection",(socket)=>{
 
 
 
-    socket.on("disconnect",(socket)=>{
+    socket.on("close",()=>{
         //socket.filter(x=>x!=socket);
         for(const [key,value] of map){
             const updatedArray = value.filter((x)=>x!==socket);
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from "react";
+
+const Home = () => {
+  const [messages,setMessages] = useState<string[]>(["hi","hello","bye"]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const wsRef = useRef<any>("");
+  useEffect(()=>{
+    const ws = new WebSocket("http://localhost:8000");
+    ws.onmessage = (event)=>{
+      setMessages(m => [...m,event.data])
+    }
+    wsRef.current = ws;
+    ws.onopen = ()=>{
+      ws.send(JSON.stringify({
+        type:"join",
+        payload:{
+          roomId:"red"
+        }
+      }))
+    };
+    return ()=>{
+      ws.close();
+    }
+  },[]);
+
+  return (
+    <div className="text-white h-screen  bg-black">
+      <div className="h-[80vh] border-blue-500 border-2  bg-red">
+          {messages.map(x=><div><span className="bg-white m-3  text-black text-xl  h-[40px] w-[10vw] flex flex-col justify-center items-center ">
+              {x}
+            </span></div>)}
+      </div>
+      <div className="flex flex-row bg-blue-50">
+        <input ref={inputRef} type="text" className="w-[90vw] p-4 text-xl text-black" name="" id="" />
+        <button onClick={()=>{
+          const value = inputRef.current?.value;
+          wsRef.current.send(JSON.stringify({
+            type:"chat",
+            payload:{
+              message:value
+            }
+          }))
+        }} className="w-[10vw] cursor-pointer text-xl font-bold  bg-blue-900 text-blue">Send</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
